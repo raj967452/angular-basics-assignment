c84e6f7b514a48628502808a9dee01bdca898026
DESKTOP-UA7RFN3\Admin <rajkumar967452@gmail.com>
2018-10-19 13:36:20 +0530

    assigment7

--- src/app/component/assigments/template-form/template-form.component.ts
+++ src/app/component/assigments/template-form/template-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Iforms } from '../../../services/forms.model';
   import { from } from 'rxjs';
 
@@ -9,23 +9,30 @@ import { Iforms } from '../../../services/forms.model';
   styleUrls: ['./template-form.component.css']
 })
 export class TemplateFormComponent implements OnInit {
-  @ViewChild('formData') loginForm = NgForm;
-  subScription = ['Basic', 'Advance', 'Pro'];
-  defaultSubScription = 'Advance';
-  subscriptionData = {
+  projectStatus = ['Stable', 'Critical', 'Finished'];
+  defaultProjectStatus = 'Stable';
+  projectStatusData = {
+    projectName: '',
     emailId: '',
-    password: '',
-    subscriptiontype: ''
+    projectStatusType: ''
   };
   submitted = false;
+
+  projectForm = new FormGroup({
+    'projectName': new FormControl('', Validators.required),
+    'email': new FormControl('', Validators.required),
+    'selectProjectStatus': new FormControl('', Validators.required)
+  });
+
   constructor() { }
 
   ngOnInit() {
+    console.log(this.projectForm);
   }
 
   onSubmit() {
     this.submitted = true;
-    console.log(this.loginForm);
+    console.log(this.projectForm);
 
    // this.loginForm.reset();
   }
